ac30837d1f3ac9cde9fc5c60867c6cae86cd505d
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-09-30 14:40:11 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,7 +1,13 @@
 alert("Initialized.");
-console.log("Sub Timer Installed.");
+console.log("CONSOLE: Sub Timer Installed.");
+var a = prompt("How many minutes should the interval be?");
+var b = a * 60 * 1000;
+if (b == 0) {
+    b = 15 * 60 * 1000;
+    alert(b);
+}
 setInterval(function(){ 
-	for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 5; i++) {
 			     alert("TAKE AN EYE BREAK"); 
-	}
-}, 900000);
+    }
+}, b);
